52fcaeca58404f908c9c4dbf4db3651585cfb8e5
PolliV <77505747+PolliV@users.noreply.github.com>
2023-04-09 23:33:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,38 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <string>
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[100];
+  int top;
+
+ public:
+  TStack() :top(-1) {}
+  void push(T value) {
+    if (isFull()) {
+    throw std::string("Full");
+    } else {
+    arr[++top] = value;
+    }
+  }
+  const T& pop() {
+    if (isEmpty()) {
+    throw std::string("Eempty");
+    } else {
+    return arr[top--];
+    }
+  }
+  bool isEmpty()const {
+    return top == -1;
+  }
+  bool isFull()const {
+    return top == size - 1;
+  }
+  const T& get()const {
+    return arr[top];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
